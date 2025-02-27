96156e0e67bdb19169d9f8aa6b85ca231d9137d3
Khojiakbar <khojiakbar_kodirov@mail.ru>
2025-02-28 00:58:52 +0900

    fix: modify admin login and signup conrollers

--- src/controllers/showroom.controller.ts
+++ src/controllers/showroom.controller.ts
@@ -3,7 +3,7 @@ import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
-import Errors, { Message } from "../libs/Errors";
+import Errors, { HttpCode, Message } from "../libs/Errors";
 
 const showroomController: T = {};
 const memberService = new MemberService();
@@ -33,15 +33,19 @@ showroomController.getSignup = (req: Request, res: Response) => {
 showroomController.processSignup = async (req: AdminRequest, res: Response) => {
     try {
         console.log("processSignup");
-        
+        const file = req.file;
+        if(!file)
+            throw new Errors(HttpCode.BAD_REQUEST, Message.SOMETHING_WENT_RONG);
+
         const newMember: MemberInput = req.body;
+        newMember.memberImage = file?.path;
         newMember.memberType = MemberType.SHOWROOM;
 
         const result = await memberService.processSignup(newMember);
 
         req.session.member = result;
         req.session.save(function(){
-            res.send(result);
+            res.redirect('/admin/product/all');
         });
 
     } catch (error) {
@@ -71,7 +75,7 @@ showroomController.processLogin = async (req: AdminRequest, res: Response) => {
 
         req.session.member = result;
         req.session.save(function(){
-            res.send(result);
+            res.redirect('/admin/product/all');
         });
     } catch (error) {
         console.log("Error, processLogin:", error);
